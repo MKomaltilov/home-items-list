484a11c0ae1b1f184ce97116addb6574fe1c5c3b
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-06-09 21:05:35 +0700

    template for get_item added

--- home_items/urls.py
+++ home_items/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('items/<int:item_id>', views.get_item, name='get_item')
 ]
--- home_items/views.py
+++ home_items/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+
 from .models import Item
 
 
@@ -8,3 +10,14 @@ def index(request):
         'items': items
     }
     return render(request, 'index.html', context)
+
+
+def get_item(request, item_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+    except Item.DoesNotExist:
+        raise Http404
+    context = {
+        'item': item
+    }
+    return render(request, 'item.html', context)
