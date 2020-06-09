a69583c81d56e0b7768707a32bf3d84cc5768a8a
mko <mko@startext.ru>
2020-06-09 14:21:31 +0700

    base models Room, Category, Item added

--- home_items/models.py
+++ home_items/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='items')
+    categories = models.ManyToManyField('Category', blank=True, related_name='items')
+    rooms = models.ForeignKey('Room', null=True, blank=True, on_delete=models.SET_NULL, related_name='items')
+
+    def __str__(self):
+        return self.name
