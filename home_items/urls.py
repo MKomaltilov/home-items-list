1cde42fb35821e626a465d70346d1529bd0cdaf1
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-06-15 22:07:13 +0700

    pages for categories, rooms, category and room added

--- home_items/urls.py
+++ home_items/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.items_overview, name='items'),
-    path('items/<int:item_id>', views.get_item, name='get_item')
+    path('items/<int:item_id>', views.get_item, name='item'),
+    path('categories/', views.categories_overview, name='categories'),
+    path('categories/<int:category_id>', views.get_category, name='category'),
+    path('rooms/', views.rooms_overview, name='rooms'),
+    path('rooms/<int:room_id>', views.get_room, name='room')
 ]
--- home_items/views.py
+++ home_items/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from .models import Item, Category
+from .models import Item, Category, Room
 
 
 def items_overview(request):
-    categories = Category.objects.all()
+    items = Item.objects.all().order_by('name')
     context = {
-        'categories': categories
+        'items': items
     }
     return render(request, 'index.html', context)
 
@@ -21,3 +21,41 @@ def get_item(request, item_id):
         'item': item
     }
     return render(request, 'item.html', context)
+
+
+def categories_overview(request):
+    categories = Category.objects.all().order_by('name')
+    context = {
+        'categories': categories
+    }
+    return render(request, 'categories.html', context)
+
+
+def get_category(request, category_id):
+    try:
+        category = Category.objects.get(pk=category_id)
+    except Item.DoesNotExist:
+        raise Http404
+    context = {
+        'category': category
+    }
+    return render(request, 'category.html', context)
+
+
+def rooms_overview(request):
+    rooms = Room.objects.all().order_by('name')
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'rooms.html', context)
+
+
+def get_room(request, room_id):
+    try:
+        room = Room.objects.get(pk=room_id)
+    except Item.DoesNotExist:
+        raise Http404
+    context = {
+        'room': room
+    }
+    return render(request, 'room.html', context)
