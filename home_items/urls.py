1afe8019b5341289f1c095783becbc90a70c991a
mko <mko@startext.ru>
2020-06-16 15:34:21 +0700

    user pages added

--- home_items/urls.py
+++ home_items/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('categories/', views.categories_overview, name='categories'),
     path('categories/<int:category_id>', views.get_category, name='category'),
     path('rooms/', views.rooms_overview, name='rooms'),
-    path('rooms/<int:room_id>', views.get_room, name='room')
+    path('rooms/<int:room_id>', views.get_room, name='room'),
+    path('rooms/no-room', views.items_wo_room, name='items_wo_room'),
+    path('users/', views.users_overview, name='users'),
+    path('users/<int:user_id>', views.get_user, name='user'),
+    path('users/family/', views.family_items, name='family_items')
 ]
--- home_items/views.py
+++ home_items/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.contrib.auth.models import User
 
 from .models import Item, Category, Room
 
@@ -7,7 +8,10 @@ from .models import Item, Category, Room
 def items_overview(request):
     items = Item.objects.all().order_by('name')
     context = {
-        'items': items
+        'items': items,
+        'family_items_count': Item.objects.filter(owner=None).count(),
+        'items_wo_category_count': Item.objects.filter(categories=None).count(),
+        'items_wo_room_count': Item.objects.filter(room=None).count()
     }
     return render(request, 'index.html', context)
 
@@ -26,7 +30,10 @@ def get_item(request, item_id):
 def items_wo_category(request):
     items = Item.objects.filter(categories=None).order_by('name')
     context = {
-        'items': items
+        'items': items,
+        'family_items_count': Item.objects.filter(owner=None).count(),
+        'items_wo_category_count': Item.objects.filter(categories=None).count(),
+        'items_wo_room_count': Item.objects.filter(room=None).count()
     }
     return render(request, 'items_wo_category.html', context)
 
@@ -54,7 +61,8 @@ def get_category(request, category_id):
 def rooms_overview(request):
     rooms = Room.objects.all().order_by('name')
     context = {
-        'rooms': rooms
+        'rooms': rooms,
+        'items_wo_room_count': Item.objects.filter(room=None).count()
     }
     return render(request, 'rooms.html', context)
 
@@ -68,3 +76,45 @@ def get_room(request, room_id):
         'room': room
     }
     return render(request, 'room.html', context)
+
+
+def users_overview(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+        'family_items_count': Item.objects.filter(owner=None).count()
+    }
+    return render(request, 'users.html', context)
+
+
+def get_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except Item.DoesNotExist:
+        raise Http404
+    context = {
+        'user': user
+    }
+    return render(request, 'user.html', context)
+
+
+def family_items(request):
+    items = Item.objects.filter(owner=None).order_by('name')
+    context = {
+        'items': items,
+        'family_items_count': Item.objects.filter(owner=None).count(),
+        'items_wo_category_count': Item.objects.filter(categories=None).count(),
+        'items_wo_room_count': Item.objects.filter(room=None).count()
+    }
+    return render(request, 'family_items.html', context)
+
+
+def items_wo_room(request):
+    items = Item.objects.filter(room=None).order_by('name')
+    context = {
+        'items': items,
+        'family_items_count': Item.objects.filter(owner=None).count(),
+        'items_wo_category_count': Item.objects.filter(categories=None).count(),
+        'items_wo_room_count': Item.objects.filter(room=None).count()
+    }
+    return render(request, 'items_wo_room.html', context)
