38534a814a82d3ab005cc96f90e0b51e910e5d36
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-06-20 17:29:32 +0700

    admin part for Item and Category updated

--- home_items/admin.py
+++ home_items/admin.py
@@ -3,9 +3,19 @@ from .models import Room, Category, Item
 
 
 admin.site.register(Room)
-admin.site.register(Category)
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'room', 'owner')
+    list_display_links = ('name', 'room', 'owner')
+    list_filter = ('categories', 'room', 'owner')
+    search_fields = ('name',)
     filter_horizontal = ('categories',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+    search_fields = ('name',)
