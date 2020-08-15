9136bae1a7d35564756e06bf2e4d6a9255fe5895
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-08-15 13:43:28 +0700

    base tests for models added

--- home_items/tests/test_models.py
+++ home_items/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from django.contrib import auth
+from django.contrib.auth import get_user_model
 from home_items.models import Room, Category, Item
 
-User = auth.get_user_model()
+User = get_user_model()
 
 
 class RoomModelTest(TestCase):
@@ -11,12 +11,22 @@ class RoomModelTest(TestCase):
         self.assertEqual(room.name, 'test name')
         self.assertEqual(room.description, None)
 
+    def test_room_with_name_and_description_can_be_created(self):
+        room = Room.objects.create(name='test name', description='test description')
+        self.assertEqual(room.name, 'test name')
+        self.assertEqual(room.description, 'test description')
+
 
 class CategoryModelTest(TestCase):
     def test_default_category_with_blank_description_can_be_created(self):
-        room = Category.objects.create(name='test name')
-        self.assertEqual(room.name, 'test name')
-        self.assertEqual(room.description, None)
+        category = Category.objects.create(name='test name')
+        self.assertEqual(category.name, 'test name')
+        self.assertEqual(category.description, None)
+
+    def test_category_with_name_and_description_can_be_created(self):
+        category = Category.objects.create(name='test name', description='test description')
+        self.assertEqual(category.name, 'test name')
+        self.assertEqual(category.description, 'test description')
 
 
 class ItemModelTest(TestCase):
@@ -26,5 +36,28 @@ class ItemModelTest(TestCase):
         self.assertEqual(item.description, None)
         self.assertEqual(item.owner, None)
         self.assertEqual(item.room, None)
-        # self.assertIn(None, item.categories.all())
-        print(item.categories.all())
+        self.assertEqual(len(item.categories.all()), 0)
+
+    def test_room_user_and_categories_can_be_added_to_item(self):
+        category_one = Category.objects.create(name='category 1')
+        category_two = Category.objects.create(name='category 2')
+        category_three = Category.objects.create(name='category 3')
+        room = Room.objects.create(name='test name')
+        owner = User.objects.create(username='user', password='123')
+
+        item = Item.objects.create(
+            name='test item',
+            description='some text in description',
+            owner=owner,
+            room=room,
+        )
+        item.categories.set([category_one, category_three])
+        item.save()
+
+        self.assertEqual(item.name, 'test item')
+        self.assertEqual(item.description, 'some text in description')
+        self.assertEqual(item.owner.username, 'user')
+        self.assertEqual(item.room.name, 'test name')
+        self.assertIn(category_one, item.categories.all())
+        self.assertIn(category_three, item.categories.all())
+        self.assertNotIn(category_two, item.categories.all())
