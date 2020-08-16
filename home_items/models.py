3ed2ba824871a8624d403e15af9e5f827cee35fe
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-08-16 12:49:05 +0700

    small cosmetic changes in models

--- home_items/models.py
+++ home_items/models.py
@@ -8,8 +8,8 @@ User = get_user_model()
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
     list = models.ForeignKey('ItemList', on_delete=models.CASCADE, related_name='rooms')
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,8 +18,8 @@ class Room(models.Model):
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
     list = models.ForeignKey('ItemList', on_delete=models.CASCADE, related_name='categories')
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -31,11 +31,11 @@ class Category(models.Model):
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    list = models.ForeignKey('ItemList', on_delete=models.CASCADE, related_name='items')
     description = models.TextField(null=True, blank=True)
     owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='items')
     categories = models.ManyToManyField('Category', blank=True, related_name='items')
     room = models.ForeignKey('Room', null=True, blank=True, on_delete=models.SET_NULL, related_name='items')
-    list = models.ForeignKey('ItemList', on_delete=models.CASCADE, related_name='items')
 
     def __str__(self):
         return self.name
