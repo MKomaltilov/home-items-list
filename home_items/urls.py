9eb19919b4e28c5f7fc9726c1e16b9141d43a884
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-08-16 12:19:46 +0700

    fix: id's in urls changed to uuid

--- home_items/urls.py
+++ home_items/urls.py
@@ -4,12 +4,12 @@ from home_items.views import items, categories, rooms, users
 
 urlpatterns = [
     path('', items.items_overview, name='items'),
-    path('items/<int:item_id>', items.get_item, name='item'),
+    path('items/<uuid:item_id>', items.get_item, name='item'),
     path('items/no-category', items.items_wo_category, name='items_wo_category'),
     path('categories/', categories.categories_overview, name='categories'),
-    path('categories/<int:category_id>', categories.get_category, name='category'),
+    path('categories/<uuid:category_id>', categories.get_category, name='category'),
     path('rooms/', rooms.rooms_overview, name='rooms'),
-    path('rooms/<int:room_id>', rooms.get_room, name='room'),
+    path('rooms/<uuid:room_id>', rooms.get_room, name='room'),
     path('rooms/no-room', items.items_wo_room, name='items_wo_room'),
     path('users/', users.users_overview, name='users'),
     path('users/<int:user_id>', users.get_user, name='user'),
